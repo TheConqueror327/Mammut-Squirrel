550d9d8d75aaea0045d535f5fe1dc29a935b18ff
TheConqueror327 <mark.erdohelyi@gmail.com>
2023-04-16 18:12:39 +0200

    Több akadály mozgásának egybehangolása

--- script.js
+++ script.js
@@ -12,8 +12,23 @@ camera.position.set(0.015, 3, 7.5);
 const textureLoader = new THREE.TextureLoader();
 scene.background = textureLoader.load('backgrounds/hatterjo.svg');
 
+const models = ['models/elephant.stl', 'models/squirrel.stl', 'models/faronk.stl', 'models/stone.stl', 'models/lejto2.stl', 'models/deszka.stl', 'models/coin2.stl'];
+let loaded = 0;
+let toLoad = models.length;
 var mammut, squirrel, oakLog, stone, downHill, oakBoard, coin;
 const hillGroup = new THREE.Group();
+var barrierObjects = [];
+
+function checkModels() {
+    loaded++;
+    if (loaded == toLoad) {
+        startEventListening();
+        init();
+        game3D();
+        updateBarriers();
+        console.log(activeBarriers);
+    }
+}
 
 function load3DModels() {
     const loader = new STLLoader();
@@ -26,6 +41,7 @@ function load3DModels() {
             mammut.rotation.y = Math.PI / 1.2;
             mammut.position.set(2, 0, /*5.3*/7);
             scene.add(mammut);
+            checkModels();
         }
     );
     loader.load(
@@ -37,6 +53,7 @@ function load3DModels() {
             squirrel.rotation.set(Math.PI / 2, Math.PI, 0);
             squirrel.position.set(0, 0, 1);
             scene.add(squirrel);
+            checkModels();
         }
     );
     loader.load(
@@ -47,6 +64,9 @@ function load3DModels() {
             oakLog.rotation.z = Math.PI / 2;
             oakLog.scale.set(1 / 7 * dim3.x, 1 / 7 * dim3.x, 1 / 7 * dim3.x);
             oakLog.position.set(Math.round(Math.random() - 1), 0.25, -50);
+            barrierObjects.push(oakLog);
+            console.log(barrierObjects);
+            checkModels();
         }
     );
     loader.load(
@@ -57,6 +77,9 @@ function load3DModels() {
             stone.rotation.y = Math.PI / 2;
             stone.scale.set(1 / dim4.x, 1 / dim4.x, 1 / dim4.x);
             stone.position.set(Math.round(Math.random() * 2 - 1), 0, -50);
+            barrierObjects.push(stone);
+            console.log(barrierObjects);
+            checkModels();
         }
     );
     loader.load(
@@ -67,6 +90,7 @@ function load3DModels() {
             downHill.scale.set(1 / dim5.x, 1 / dim5.x, 1 / dim5.x);
             downHill.position.set(0, 0.5, 3);
             hillGroup.add(downHill);
+            checkModels();
         }
     );
     loader.load(
@@ -78,6 +102,7 @@ function load3DModels() {
             oakBoard.position.set(0, 0.5, 2.9);
             oakBoard.rotation.set(Math.PI / -2, 0, Math.PI);
             hillGroup.add(oakBoard);
+            checkModels();
         }
     )
     loader.load(
@@ -87,129 +112,139 @@ function load3DModels() {
             let dim7 = new THREE.Box3().setFromObject(coin).getSize(new THREE.Vector3());
             coin.scale.set(1 / (2 * dim7.x), 1 / (2 * dim7.x), 1 / (2 * dim7.x));
             coin.position.y = 0.25;
+            checkModels();
         }
     )
 }
 
 load3DModels();
 
-var oakLogRotations = [0, Math.PI / 2];
-var motionDuration = 200;
 var rAF;
-var points = 0;
 let isStopped = false;
 const menu = document.getElementById('menu');
 
-addEventListener("keydown", event => {
-    if ((event.code === 'KeyD') && (mammut.position.x <= 2)) {
-        animateObjectMotion(mammut, {x: mammut.position.x + 1}, motionDuration);
-        animateObjectMotion(squirrel, {x: squirrel.position.x + 1}, motionDuration);
-        animateObjectMotion(camera, {x: camera.position.x + 1}, motionDuration);
-    }
-    if ((event.code === 'KeyA') && (mammut.position.x >= 2)) {
-        animateObjectMotion(mammut, {x: mammut.position.x - 1}, motionDuration);
-        animateObjectMotion(squirrel, {x: squirrel.position.x - 1}, motionDuration);
-        animateObjectMotion(camera, {x: camera.position.x - 1}, motionDuration);
-    }
-    if (event.code === 'Space') {
-        animateObjectMotion(mammut, {y: 1}, motionDuration);
-        animateObjectMotion(squirrel, {y: 1}, motionDuration);
-        setTimeout(() => {
-            animateObjectMotion(mammut, {y: 0}, motionDuration);
-            animateObjectMotion(squirrel, {y: 0}, motionDuration);
-        }, 200);   
-    }
-    if (event.code === 'Escape') {
-        if (!isStopped) {
-            cancelAnimationFrame(rAF);
-            menu.style.display = 'block';
-            document.getElementById('game').style.opacity = 0.5;
-            isStopped = true;
-        } else {
-            game3D();
-            menu.style.display = 'none';
-            document.getElementById('game').style.opacity = 1;
-            isStopped = false;
+function startEventListening() {
+    addEventListener("keydown", event => {
+        if ((event.code === 'KeyD') && (mammut.position.x <= 2)) {
+            animateObjectMotion(mammut, {x: mammut.position.x + 1}, motionDuration);
+            animateObjectMotion(squirrel, {x: squirrel.position.x + 1}, motionDuration);
+            animateObjectMotion(camera, {x: camera.position.x + 1}, motionDuration);
         }
-    }
-});
+        if ((event.code === 'KeyA') && (mammut.position.x >= 2)) {
+            animateObjectMotion(mammut, {x: mammut.position.x - 1}, motionDuration);
+            animateObjectMotion(squirrel, {x: squirrel.position.x - 1}, motionDuration);
+            animateObjectMotion(camera, {x: camera.position.x - 1}, motionDuration);
+        }
+        if (event.code === 'Space') {
+            animateObjectMotion(mammut, {y: 1}, motionDuration);
+            animateObjectMotion(squirrel, {y: 1}, motionDuration);
+            setTimeout(() => {
+                animateObjectMotion(mammut, {y: 0}, motionDuration);
+                animateObjectMotion(squirrel, {y: 0}, motionDuration);
+            }, 200);
+        }
+        if (event.code === 'Escape') {
+            if (!isStopped) {
+                cancelAnimationFrame(rAF);
+                clearInterval(barrierInterval);
+                menu.style.display = 'block';
+                document.getElementById('game').style.opacity = 0.5;
+                isStopped = true;
+            } else {
+                game3D();
+                menu.style.display = 'none';
+                document.getElementById('game').style.opacity = 1;
+                isStopped = false;
+            }
+        }
+    });
+    window.addEventListener('resize', () => {
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    });
+}
 
-window.addEventListener('resize', () => {
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-});
+var oakLogRotations, motionDuration;
+let points, increment, speed;
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-
-var floorGeometry = new THREE.BoxGeometry(3.3, 0.1, 75);
-var floorMaterial = new THREE.MeshStandardMaterial({color: 0xffffff});
-
-var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-scene.add(floor);
-floor.position.set(0, -0.1, -24.5);
-
-scene.add(new THREE.HemisphereLight(0xffffff, 0x000000));
-renderer.render(scene, camera);
-
-let increment = 0.2;
+function init() {
+    oakLogRotations = [0, Math.PI / 2];
+    motionDuration = 200;
+    points = 0;
+    speed = 2000;
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    var floor = new THREE.Mesh(new THREE.BoxGeometry(3.3, 0.1, 75), new THREE.MeshStandardMaterial({color: 0xffffff}));
+    floor.position.set(0, -0.1, -24.5);
+    scene.add(floor);
+    scene.add(new THREE.HemisphereLight(0xffffff, 0x000000));
+    increment = 0.2;
+    hillGroup.rotation.y = Math.PI;
+    hillGroup.position.set(Math.round(Math.random() * 2 - 1), 0, -50);
+    barrierObjects.push(hillGroup);
+}
 
 function animateObjectMotion(object, coordinates, duration) {
     var tween = new TWEEN.Tween(object.position).to(coordinates, duration).easing(TWEEN.Easing.Quadratic.InOut);
     tween.start();
 }
 
-hillGroup.rotation.y = Math.PI;
-hillGroup.position.set(Math.round(Math.random() * 2 - 1), 0, -50);
-var barrierObjects, randomBarrier, activeBarrier;
+let randomBarrier = Math.round(Math.random() * 2);
+let activeBarriers = [];
+let activeBarrierGroup = [];
+var barrierInterval;
 
-function initializeBarriers() {
-    barrierObjects = [oakLog, stone, hillGroup];
-    randomBarrier = Math.round(Math.random() * 2);
-    activeBarrier = barrierObjects[randomBarrier];
-    scene.add(activeBarrier);
+function updateBarriers() {
+    barrierInterval = setInterval(
+    () => {
+        initializeBarrier(barrierObjects[randomBarrier].clone(), randomBarrier);
+        randomBarrier = Math.round(Math.random() * 2);
+        console.log(activeBarriers);
+    }, speed);
 }
 
-var game3D = () => {
+function initializeBarrier(object, randBarr) {
+    object.position.z = -50;
+    if (randBarr == 0) {
+        object.position.x = Math.round(Math.random()) - 1;
+        let randomRotation = Math.round(Math.random());
+        object.rotation.z = oakLogRotations[randomRotation];
+        if (randomRotation == 0) {
+            object.position.y = 1.6;
+        } else {
+            object.position.y = 0.5;
+        }
+    } else {
+        object.position.x = Math.round(Math.random() * 2 - 1);
+    }
+    activeBarrierGroup = [object, randBarr];
+    activeBarriers.push(activeBarrierGroup);
+    scene.add(object);
+}
+
+function game3D() {
 	rAF = requestAnimationFrame(game3D);
 	renderer.render(scene, camera);
     document.getElementById('points').innerHTML = `Pontszám: ${points}`;
     points++;
     try {
-        if (activeBarrier.position.z != -1) {
-            activeBarrier.position.z += increment;
-        }
-        if ((activeBarrier.position.z > 1) && (Math.round(activeBarrier.position.x) == Math.round(squirrel.position.x)) && (randomBarrier == 2) && (squirrel.position.y < 1.1)) {
-            squirrel.position.y = (1.093 * (activeBarrier.position.z / 2 - squirrel.position.z / 2)) / 2.609;
-        }
-        if ((activeBarrier.position.z > 2.609) && (Math.round(activeBarrier.position.x) == Math.round(squirrel.position.x)) && (randomBarrier == 2)) {
-            animateObjectMotion(squirrel, {y: squirrel.position.y - 1}, 500);
-        }
-        if (activeBarrier.position.z > 10) {
-            activeBarrier.position.z = -50;
-            scene.remove(activeBarrier);
-            randomBarrier = Math.round(Math.random() * 2);
-            activeBarrier = barrierObjects[randomBarrier];
-            scene.add(activeBarrier);
-            if (randomBarrier == 0) {
-                activeBarrier.position.x = Math.round(Math.random()) - 1;
-                let randomRotation = Math.round(Math.random());
-                activeBarrier.rotation.z = oakLogRotations[randomRotation];
-                if (randomRotation == 0) {
-                    activeBarrier.position.y = 1.5;
-                } else {
-                    activeBarrier.position.y = 0.5;
-                }
-            } else {
-                activeBarrier.position.x = Math.round(Math.random() * 2 - 1);
+        for (let i = 0; i < activeBarriers.length; i++) {
+            activeBarriers[i][0].position.z += increment;
+            if ((activeBarriers[i][0].position.z > 1) && (Math.round(activeBarriers[i][0].position.x) == Math.round(squirrel.position.x)) && (activeBarriers[i][1] == 2) && (squirrel.position.y < 1.1)) {
+                squirrel.position.y = (1.093 * (activeBarriers[i][0].position.z / 2 - squirrel.position.z / 2)) / 2.609;
+            }
+            if ((activeBarriers[i][0].position.z > 2.609) && (Math.round(activeBarriers[i][0].position.x) == Math.round(squirrel.position.x)) && (activeBarriers[i][1] == 2)) {
+                animateObjectMotion(squirrel, {y: squirrel.position.y - 1}, 500);
+            }
+            if (activeBarriers[i][0].position.z > 10) {
+                scene.remove(activeBarriers[i][0]);
+                activeBarriers.splice(i, 1);
             }
         }
-    } catch (error) {
+    }
+    catch (error) {
         console.log(error);
-        initializeBarriers();
     }
     TWEEN.update();
 };
-
-game3D();
